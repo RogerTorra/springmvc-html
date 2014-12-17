54b984e72eb7ae60531971a8fa4ebd0ab8362880
RogerTorra <elrogertorra@gmail.com>
2014-12-17 16:05:46 +0100

    XML conection Test

---
+++ src/main/java/cat/udl/eps/softarch/hello/controller/GreetingHomeController.java
@@ -0,0 +1,7 @@
+package cat.udl.eps.softarch.hello.controller;
+
+/**
+ * Created by roger on 08/10/2014.
+ */
+public class GreetingHomeController {
+}
---
+++ src/test/java/cat/udl/eps/softarch/hello/XMLConnectionTest.java
@@ -0,0 +1,7 @@
+package cat.udl.eps.softarch.hello;
+
+/**
+ * Created by roger on 17/12/2014.
+ */
+public class XMLConnectionTest {
+}
